86b719da98501a00982c0c166fa58e3fcfcd9d59
DeniLaFauci <denislafauci@gmail.com>
2020-02-05 12:56:19 +0100

    Added equals method to cathegory model

--- src/main/java/it/contrader/model/Category.java
+++ src/main/java/it/contrader/model/Category.java
@@ -49,5 +49,26 @@ public class Category
 		return id_cat + "\t" + name;
 	}
 	
-	/* TO DO EQUALS */ 
+	/* EQUALS METHOD */
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id_cat != other.id_cat)
+			return false;
+		if (name == null)
+		{
+			if (other.name != null)
+				return false;
+		}
+		else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 }
