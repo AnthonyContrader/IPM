27131af527c084ff33f0bf9dd7650bee71aaca3c
DeniLaFauci <denislafauci@gmail.com>
2020-02-05 11:19:55 +0100

    Added category model

---
+++ src/main/java/it/contrader/model/Category.java
@@ -0,0 +1,52 @@
+package it.contrader.model;
+
+public class Category
+{
+	/* VARIABLES */
+	private int id_cat;
+	private String name;
+	
+	/* CONSTRUCTORS */
+	public Category ()
+	{
+	}
+	
+	public Category (String name) 
+	{
+		this.name = name;
+	}
+	
+	public Category (int id_cat, String name) 
+	{
+		this.id_cat = id_cat;
+		this.name = name;
+	}
+	
+	/* GETTER AND SETTER METHODS */
+	public int getIdCat() 
+	{
+		return this.id_cat;
+	}
+	
+	public void setIdCat(int id_cat) 
+	{
+		this.id_cat = id_cat;
+	}
+	
+	public String getName() 
+	{
+		return this.name;
+	}
+	
+	public void setName() 
+	{
+		this.name = name;
+	}
+	
+	public String toString() 
+	{
+		return id_cat + "\t" + name;
+	}
+	
+	/* TO DO EQUALS */ 
+}
