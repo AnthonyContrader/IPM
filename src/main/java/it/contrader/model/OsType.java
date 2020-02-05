be72d8f04e0a9c9a0906eb04bc118266c4dc7c60
Aleks451 <60605622+Aleks451@users.noreply.github.com>
2020-02-05 11:53:15 +0100

    added equals method to osType model

--- src/main/java/it/contrader/model/OsType.java
+++ src/main/java/it/contrader/model/OsType.java
@@ -1,5 +1,55 @@
 package it.contrader.model;
 
 public class OsType {
+	
+	private int id_ostype;
+	private String name;
+	private String command;
+	
+	public OsType() { }
+	
+	public OsType(String name, String command) {
+		super();
+		this.name = name;
+		this.command = command;
+	}
 
+	public OsType(int id, String name, String command) {
+		super();
+		this.id_ostype = id;
+		this.name = name;
+		this.command = command;
+	}
+
+	public int getId() {
+		return id_ostype;
+	}
+
+	public void setId(int id) {
+		this.id_ostype = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	@Override
+	public String toString() {
+		return "OsType [id=" + id_ostype + ", name=" + name + ", command=" + command + "]";
+	}
+	
+	/**public boolean equals (Object obj) { }
+	*
 }
