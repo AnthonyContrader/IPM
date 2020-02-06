26c418fc5796df3907935053cfdbf078289859e1
Aleks451 <60605622+Aleks451@users.noreply.github.com>
2020-02-06 10:24:50 +0100

    src/main/java/it/contrader/view/ostype/ added

---
+++ src/main/java/it/contrader/view/ostype/OsTypeDeleteView.java
@@ -0,0 +1,38 @@
+package it.contrader.view.ostype;
+
+import it.contrader.controller.Request;
+import it.contrader.main.MainDispatcher;
+import it.contrader.view.AbstractView;
+
+public class OsTypeDeleteView extends AbstractView{
+	private Request request;
+
+	private int id;
+	private final String mode = "DELETE";
+	
+	public OsTypeDeleteView () { }
+
+	@Override
+	public void showResults(Request request) {
+		if (request!=null) {
+			System.out.println("Cancellazione andata a buon fine.\n");
+			MainDispatcher.getInstance().callView("OsType", null);		
+		}
+	}
+		
+
+	@Override
+	public void showOptions() {
+		System.out.println("Inserisci id del sistema operativo:");
+		id = Integer.parseInt(getInput());
+	}
+
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("id", id);
+		request.put("mode", mode);
+		MainDispatcher.getInstance().callAction("OsType", "doControl", request);
+	}
+	
+}
---
+++ src/main/java/it/contrader/view/ostype/OsTypeReadView.java
@@ -0,0 +1,38 @@
+package it.contrader.view.ostype;
+
+import it.contrader.controller.Request;
+import it.contrader.dto.OsTypeDTO;
+import it.contrader.main.MainDispatcher;
+import it.contrader.view.AbstractView;
+
+public class OsTypeReadView extends AbstractView{
+	private int id;
+	private Request request;
+	private final String mode = "READ";
+	
+	public OsTypeReadView () {}
+
+	@Override
+	public void showResults(Request request) {
+		if (request != null) {
+			OsTypeDTO ostype = (OsTypeDTO) request.get("ostype");
+			System.out.println(ostype);
+			MainDispatcher.getInstance().callView("OsType", null);
+		}		
+	}
+
+	@Override
+	public void showOptions() {
+		System.out.println("Inserisci l'ID del sistema operativo:");
+		id = Integer.parseInt(getInput());		
+	}
+
+	@Override
+	public void submit() {
+		request = new Request();
+		request.put("id", id);
+		request.put("mode", mode);
+		MainDispatcher.getInstance().callAction("OsType", "doControl", request);		
+	}
+
+}
