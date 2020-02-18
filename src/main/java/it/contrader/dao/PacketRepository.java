95068c483bf8e7aa57c9d7c96ea3233031e8fc62
less-hub <alessiocellucci.dev@gmail.com>
2020-02-18 14:33:45 +0100

    Added annotation to repo

--- src/main/java/it/contrader/dao/PacketRepository.java
+++ src/main/java/it/contrader/dao/PacketRepository.java
@@ -1,9 +1,14 @@
 package it.contrader.dao;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import it.contrader.model.Packet;
 
+@Repository
+@Transactional
 public interface PacketRepository extends CrudRepository<Packet, Long> {
 	
 	Packet findByName(String name);
